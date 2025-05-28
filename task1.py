86b2eda26dc7f9e7719829f1bb92b80587031368
Yevhenii <ye.kozolup@udu.edu.ua>
2025-05-28 11:17:21 +0300

    Create file task1.py

--- task1.py
+++ task1.py
@@ -1,2 +1,18 @@
-a = int(input())
-print(a+1)
+def select(grades : list, passgrade = 60.0 ) -> list:
+    """
+    Функція приймає на вхід список з дійсних чисел та повертає відфільтрований список із значень більших за введене число
+    """
+    result = []
+    for el in grades:
+        if el >= passgrade:
+            result.append(el)
+    return result
+
+gradebook = {"Yevhenii": [60, 56, 89, 46, 100],
+             "Sonya" : [54, 43, 100, 86],
+             "Danya" : [30, 10, 3, 100, 100],
+             "Ruslan" : [100, 100, 100, 100, 100]}
+
+for key in gradebook:
+    a = select(gradebook[key])
+    print(f"{key}:", *a, f"Average : {sum(a)/len(a)}")
